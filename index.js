c3f611cf60b680b10003dc437724c429b79499c8
Amanda Wehrman <arwehrman@gmail.com>
2018-01-21 18:00:26 +0000

    automatically backed up by learn

--- index.js
+++ index.js
@@ -6,18 +6,16 @@ function BoardMember(name, homeState, training) {
   BoardMemeber.prototype.veto = function(){
      console.log('No, I must disagree')
    }
-//   this.approve = function(){
-//     return 'You can do that!'
-//   }
-//   this.doCharity = function(){
-//     return "I like to help people."
-//   }
-//
-//   this.releasePressStatement = function(){
-//     return "You will see great things from Scuber."
-//   }
-//
-//   this.sayHi = function(){
-//     return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
-//   }
-// }
+   BoardMember.prototype.approve = function(){
+     console.log('You can do that!')
+   }
+  BoardMember.prototype.doCharity = function(){
+    console.log("I like to help people.")
+  }
+  BoardMember.prototype.releasePressStatement = function(){
+    console.log("You will see great things from Scuber.")
+  }
+  BoardMember.prototype.sayHi = function(){
+   return `Hi, my name is ${this.name}. I am from ${this.homeState}, and I was trained in ${this.training}.`
+  }
+ }
